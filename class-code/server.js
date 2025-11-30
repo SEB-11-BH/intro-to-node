6fe8e161d34ad067551e1360012bac6820b71b30
omarakamal <omaraakamal@gmail.com>
2025-11-30 11:01:39 +0300

    update

---
+++ class-code/server.js
@@ -0,0 +1,12 @@
+console.log('Test')
+
+const colors = require('colors')
+
+
+// to use a package
+
+// 1. npm install package-name
+
+// 2. const packageName = require('package-name')
+
+console.log(colors.rainbow('Testing colors package'))
